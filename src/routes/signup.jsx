7afa94f5806658e500b5ef57d3b033f5f6dd5386
Noah Burton <nburton115@gmail.com>
2024-01-24 15:50:52 -0700

    signup.js: google log in

--- src/routes/signup.jsx
+++ src/routes/signup.jsx
@@ -5,12 +5,25 @@ import Col from 'react-bootstrap/Col';
 import Form  from 'react-bootstrap/Form';
 import Button  from 'react-bootstrap/Button';
 
+
+import { GoogleLogin } from '@react-oauth/google';
+
 export default function Signup() {
+
+    const responseMessage = (response) => {
+        console.log(response);
+    };
+    const errorMessage = (error) => {
+        console.log(error);
+    };
+
     return (
         <Container>
             <Row>
                 {/* Google auth here */}
                 <h1>Google stuff!</h1>
+                <br />
+                <GoogleLogin onSuccess={responseMessage} onError={errorMessage} clientId='272155549288-5m0eal6p2p7ufof91m71sb3ijcqkkasp.apps.googleusercontent.com' />
             </Row>
         </Container>
     )
