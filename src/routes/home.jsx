5b3a7c4940209578b98687d1e01715ad9d02ff10
Noah Burton <nburton115@gmail.com>
2024-01-25 15:07:23 -0700

    layout adjustment

--- src/routes/home.jsx
+++ src/routes/home.jsx
@@ -1,7 +1,3 @@
-// import { Outlet } from "react-router-dom";
-
-
-
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
@@ -10,10 +6,9 @@ import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 
-  
 const Header = () => {
   return (
-    <Navbar >
+    <Navbar>
       <Navbar.Brand href="">My Next Song</Navbar.Brand>
       <Nav className="mr-auto">
         <Nav.Link href="">Home</Nav.Link>
@@ -23,36 +18,36 @@ const Header = () => {
   );
 }
 
-  
 export default function Home() {
-
-    return (
-      <Container>
-        <Header />
-        <Row>
-          <Col xs={12} md={4} order={1}>
-            <Card>
-              <Card.Body>
-                <Card.Title>current song</Card.Title>
-                <Card.Text>
-                  This is some text within a card body.
-                </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col xs={12} md={8} order={0}>
-            <Card>
-              <Card.Body>
-                <Card.Title>Main Content</Card.Title>
-                <Card.Text>
-                  This is some text within a card body.
-                </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
-              </Card.Body>
-            </Card>
-          </Col>
-        </Row>
-      </Container>
-    );
-  }
+  return (
+    <Container>
+      <Header />
+      <Row>
+        <Col xs={12}>
+          <Card>
+            <Card.Body>
+              <Card.Title>Account Info</Card.Title>
+              <Card.Text>
+                This is some text within a card body.
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={12}>
+          <Card>
+            <Card.Body>
+              <Card.Title>Main Content</Card.Title>
+              <Card.Text>
+                This is some text within a card body.
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
