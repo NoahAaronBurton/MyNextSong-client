ab10807af741f643c09218de23ae1b35a91f9694
Noah Burton <nburton115@gmail.com>
2024-01-26 17:27:03 -0700

    new google auth strategy

--- src/routes/login.jsx
+++ src/routes/login.jsx
@@ -1,35 +1,14 @@
 import Container from 'react-bootstrap/Container';
-import Card from 'react-bootstrap/Card';
 import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Form  from 'react-bootstrap/Form';
 import Button  from 'react-bootstrap/Button';
 
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
-
-
-import { GoogleLogin } from '@react-oauth/google';
-
 export default function Login() {
-    const navigate = useNavigate();
 
-    const responseMessage = (response) => {
-        console.log(response.credential);
+    //todo: use api endpoints for user auth status, info, etc
 
-        
-        axios.post('http://localhost:3000/api/auth/google', { token: response.credential })
-        .then(res => {
-            console.log(res.data);
-            navigate('/home');  // Redirect to '/home'
-        })
-        .catch(err => console.error(err));
-
-
-    };
-    const errorMessage = (error) => {
-        console.log(error);
-    };
+    const googleAuth = () => {
+        window.open(`${import.meta.env.VITE_API_URL}/auth/google/callback`, '_self');
+    }
 
     return (
         <Container>
@@ -37,7 +16,7 @@ export default function Login() {
                 {/* Google auth here */}
                 <h1>Google stuff!</h1>
                 <br />
-                <GoogleLogin onSuccess={responseMessage} onError={errorMessage} clientId='272155549288-5m0eal6p2p7ufof91m71sb3ijcqkkasp.apps.googleusercontent.com' />
+                <Button onClick={googleAuth}>Google Auth</Button>
             </Row>
         </Container>
     )
